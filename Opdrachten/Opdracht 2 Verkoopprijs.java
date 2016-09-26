b8210ac84b563a10545e610bbd5939fd26f37b64
Harwinder <llllIHARWINDERIllll@users.noreply.github.com>
2016-09-26 18:35:48 +0200

    ツ

--- Opdrachten/Opdracht 2 Verkoopprijs.java
+++ Opdrachten/Opdracht 2 Verkoopprijs.java
@@ -1,6 +1,6 @@
 /*
  * Het doel van dit programma is om een Verkoopprijs Met en zonder BTW te berekenen D.M.V. de winstpercentage en inkoopprijs
- * @author 	ツ
+ * @author ツ
  */
 package verkoopprijsopdracht;
 import java.util.Scanner; 
